616da25088123cb0bd9430e620a971e20e666720
kofi_m1 <1436100728@qq.com>
2026-02-12 17:25:02 +0800

    deving

---
+++ songs/songs_libs.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    : songs_libs.py
+@Time    : 2026/02/12 17:09:18
+@Author  : Kofi Wang
+@Contact : wonkefei@gmail.com
+'''
+
+
+def id_process(df, is_ost=False, is_words=True):
+    # 曲目，专辑添加唯一id
+    df = df.copy()
+    df['song_id_unique'] = 'song' + df['song_id'].astype(str)
+    # df_album_fixed = df[['album_id', 'album_fixed']].drop_duplicates(subset=['album_fixed'], keep='first').reset_index(drop=True)
+    # df_album_fixed['album_id_unique'] = 'album' + df_album_fixed['album_id'].astype(int).astype(str)
+    # df_album_fixed = df_album_fixed[['album_id_unique', 'album_fixed']]
+    df['song_year'] = df['publish_date'].str.split('-').str[0]
+    if is_ost:
+        df['album_fixed'] = df['song_year'] + '年'
+        df['album_id_unique'] = 'album' + df['song_year']
+        df['legend_type'] = df['song_year'] + '年'
+    else:
+        df['album_fixed'] = df['album_name']
+        df['album_id_unique'] = 'album' + df['album_id'].astype(str)
+        df['legend_type'] = df['album_name']
+    # 生成年份排序映射（album_order）
+    # 使用 factorize 可以直接一步生成按顺序排列的编码
+    # 如果必须按年份数值排序，则先排序再 factorize
+    unique_years = sorted(df['song_year'].unique())
+    year_to_order = {year: i for i, year in enumerate(unique_years)}
+    df['album_order'] = df['song_year'].map(year_to_order)
+    # df['legend_order'] = df['legend_type']
+    # 词唯一id
+    if is_words:
+        df_words = df[['word',
+                       'pos']].drop_duplicates().reset_index(drop=False)
+        df_words['word_id'] = 'word' + df_words['index'].astype(str)
+        df_words = df_words.drop('index', axis=1).reset_index(drop=True)
+        df = df.merge(df_words, on=['word', 'pos'], how='left')
+    return df
+
+
+if __name__ == "__main__":
+    print("This is a library of functions for processing song data, not meant to be run directly.")
